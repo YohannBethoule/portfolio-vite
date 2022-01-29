ece69b74d33552bbe9cf75b28a5cc14c9970b25c
Yohann Bethoule <yoyoteva@gmail.com>
2022-01-29 16:06:09 +0100

    updated front

--- src/components/ArticleSkills.vue
+++ src/components/ArticleSkills.vue
@@ -2,9 +2,8 @@
     <section v-for="category  in skills.data" :key="skills.title" class="pt-10">
       <AppH2 class="text-3xl">> {{category.label}}</AppH2>
       <span class="flex flex-wrap mt-5">
-        <SkillChip v-for="skill in category.skillset" class="mr-5 mb-5 text-lg">{{skill}}</SkillChip>
+        <SkillChip v-for="skill in category.skillset" dark class="mr-5 mb-5 text-lg">{{skill}}</SkillChip>
       </span>
-
     </section>
 </template>
 
--- src/components/PageInfos.vue
+++ src/components/PageInfos.vue
@@ -2,18 +2,18 @@
   <div class="flex flex-col mx-auto text-center space-y-5">
     <h1 class="text-5xl md:text-9xl font-bold"> {{infos.name}}</h1>
     <AppH2>{{infos.subtitle}}</AppH2>
-    <div class="flex flex-col text-3xl space-y-2">
-      <h3>
+    <div class="flex flex-col space-y-2">
+      <AppH3>
         <AppLink :href="'mailto:'+infos.mail">
           <font-awesome-icon icon="envelope" class="mr-2"/>{{infos.mail}}
         </AppLink>
-      </h3>
-      <h3><font-awesome-icon icon="phone" class="mr-2"/>{{infos.phone}}</h3>
-      <h3><font-awesome-icon icon="home" class="mr-2"/>{{infos.postalCode}} {{infos.city}}, {{infos.country.toUpperCase()}}</h3>
-      <h3>
+      </AppH3>
+      <AppH3><font-awesome-icon icon="phone" class="mr-2"/>{{infos.phone}}</AppH3>
+      <AppH3><font-awesome-icon icon="home" class="mr-2"/>{{infos.postalCode}} {{infos.city}}, {{infos.country.toUpperCase()}}</AppH3>
+      <AppH3>
         <AppLink :href="infos.github" class="text-5xl m-5"><font-awesome-icon :icon="['fab', 'github']"/></AppLink>
         <AppLink :href="infos.linkedin" class="text-5xl m-5"><font-awesome-icon :icon="['fab', 'linkedin']"/></AppLink>
-      </h3>
+      </AppH3>
     </div>
 <!--    <p class="text-3xl">{{infos.description}}</p>-->
   </div>
@@ -24,6 +24,7 @@
 import {infos} from "../data.json"
 import AppH1 from "./AppH1.vue";
 import AppH2 from "./AppH2.vue";
+import AppH3 from "./AppH3.vue";
 import AppLink from "./AppLink.vue";
 </script>
 
