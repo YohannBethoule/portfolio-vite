8d30edec71568269380a278684832ace110e4594
Yohann Bethoule <yoyoteva@gmail.com>
2022-05-31 14:09:54 +0200

    updated jobs since job change

--- src/components/AppSkillCard.vue
+++ src/components/AppSkillCard.vue
@@ -10,14 +10,6 @@
       </div>
     </div>
   </div>
-  <!--  <div class="flex flex-col border border-gray p-2 rounded overflow-hidden shadow-lg text-center-->
-  <!--    w-2/5 h-22 md:w-48 md:h-32-->
-  <!--    hover:shadow-2xl">-->
-  <!--    <img v-if="image" :alt="name" :src="'/skills/' + image" class="h-6 md:h-10 mx-auto mt-3 md:mt-5">-->
-  <!--    <div class=" my-auto font-bold  md:text-xl">-->
-  <!--      {{ name }}-->
-  <!--    </div>-->
-  <!--  </div>-->
 </template>
 
 <script>
