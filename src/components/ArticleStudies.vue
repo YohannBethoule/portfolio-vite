8578885aa3cfbd5c8bed85a8e11b1b310e7c4b13
Yohann Bethoule <yoyoteva@gmail.com>
2024-09-16 11:39:10 +0200

    Update jobs display (#29)

--- src/components/ArticleStudies.vue
+++ src/components/ArticleStudies.vue
@@ -1,17 +1,18 @@
 <template>
     <section v-for="formation in studies.data" :key="formation.title">
-      <AppSection :title="formation.title"
+        <Article :title="formation.title"
         :date="formation.date"
                   :place="formation.place">
         <div v-for="subject in formation.main_subjects">
           {{subject}}
         </div>
-      </AppSection>
+        </Article>
     </section>
 </template>
 
 <script setup>
-import AppSection from "./AppSection.vue";
+import Article from "./Article.vue";
+
 const props = defineProps({
   studies: Object
 })
--- src/components/Section.vue
+++ src/components/Section.vue
@@ -1,24 +1,22 @@
-<template>
-  <div :class="dark ? 'text-neutral-100 bg-black' : 'bg-neutral-100'"
-       class="min-h-screen place-content-center flex flex-col py-28 px-12 md:px-16 app-section">
-    <AppH1 v-if="dataSet.title" class="mb-10">
-      <font-awesome-icon v-if="dataSet.icon" :icon="dataSet.icon" class="mr-2"/>
-      {{ dataSet.title }}
-    </AppH1>
-    <article>
-      <slot></slot>
-    </article>
-  </div>
-</template>
-
 <script setup>
 import AppH1 from "./AppH1.vue";
 
 const props = defineProps({
-  dataSet: Object,
-  dark: Boolean,
+    dataSet: Object,
+    dark: Boolean,
 })
 </script>
+
+<template>
+    <section :class="dark ? 'text-neutral-100 bg-black' : 'bg-neutral-100'"
+             class="min-h-screen place-content-center flex flex-col py-16 px-12 md:px-16 md:py-28 app-section">
+        <AppH1 v-if="dataSet.title" class="mb-10">
+            <font-awesome-icon v-if="dataSet.icon" :icon="dataSet.icon" class="mr-2"/>
+            {{ dataSet.title }}
+        </AppH1>
+        <slot></slot>
+    </section>
+</template>
 
 <style scoped>
 </style>
